3f6b53c3114e594a47f7f7fee38ac70801a304ea
Samuel Tseng <samuel.tseng@homeday.de>
2024-12-02 15:08:53 +0100

    transform to location_prices

--- src/pipelines/transform.py
+++ src/pipelines/transform.py
@@ -65,6 +65,76 @@ data_transform_to_report_headers = """
     FROM expanded_rows er;
 """
 
+data_transform_to_location_prices = """
+    WITH price_data AS (
+        -- Extract prices based on property_type from prices_all
+        SELECT 
+            pa.aviv_geo_id,
+            pa.price_date::date,
+            CASE
+                WHEN rh.property_type = 'apartment' THEN (pa.apartment_price->>'value')::numeric
+                WHEN rh.property_type = 'house' THEN (pa.house_price->>'value')::numeric
+            END AS price,
+            CASE
+                WHEN rh.property_type = 'apartment' THEN (pa.apartment_price->>'high')::numeric
+                WHEN rh.property_type = 'house' THEN (pa.house_price->>'high')::numeric
+            END AS max,
+            CASE
+                WHEN rh.property_type = 'apartment' THEN (pa.apartment_price->>'low')::numeric
+                WHEN rh.property_type = 'house' THEN (pa.house_price->>'low')::numeric
+            END AS min,
+            CASE
+                WHEN rh.property_type = 'apartment' THEN (pa.apartment_price->>'accuracy')::numeric
+                WHEN rh.property_type = 'house' THEN (pa.house_price->>'accuracy')::numeric
+            END AS score,
+            rh.id AS report_header_id,
+            rh.name AS header_name
+        FROM prices_all pa
+        JOIN report_headers rh
+        ON pa.price_date::date = rh.date
+    ),
+    geo_data AS (
+        -- Map geo_cache information for zip_code and city_id
+        SELECT
+            gc.geo_index,
+            gc.aviv_geo_id,
+            gc.hd_geo_id,
+            CASE
+                WHEN gc.hd_geo_id = 'no_hd_geo_id_applicable' THEN gc.geo_index
+                ELSE NULL
+            END AS zip_code,
+            CASE
+                WHEN gc.hd_geo_id != 'no_hd_geo_id_applicable' THEN gc.hd_geo_id
+                ELSE NULL
+            END AS city_id
+        FROM geo_cache gc
+    )
+    INSERT INTO location_prices (
+        report_header_id, city_id, zip_code, price, unit, median, created_at, updated_at, country, max, min, score
+    )
+    SELECT 
+        pd.report_header_id,
+        gd.city_id,
+        gd.zip_code,
+        pd.price,
+        'EUR_SQM' AS unit,
+        pd.price AS median,
+        CURRENT_TIMESTAMP AS created_at,
+        CURRENT_TIMESTAMP AS updated_at,
+        'DE' AS country,
+        pd.max,
+        pd.min,
+        pd.score
+    FROM price_data pd
+    LEFT JOIN geo_data gd
+    ON pd.aviv_geo_id = gd.aviv_geo_id
+    WHERE 
+        -- Map `zip_codes` to `hd_geo_id = 'no_hd_geo_id_applicable'`
+        (pd.header_name = 'zip_codes' AND gd.hd_geo_id = 'no_hd_geo_id_applicable')
+        OR
+        -- Map `cities` to valid `hd_geo_id`
+        (pd.header_name = 'cities' AND gd.hd_geo_id != 'no_hd_geo_id_applicable');
+"""
 
 class TransformAVIVRawToHDPrices:
     pass
