a3c372c4e131442f4184d61631504266f7fc8812
Samuel Tseng <samuel.tseng@homeday.de>
2024-12-04 16:29:09 +0100

    refactor APIClient

--- src/api_client.py
+++ src/api_client.py
@@ -1,49 +1,88 @@
 import logging
 import aiohttp
-from typing import Dict
+from typing import Dict, List
 from aiohttp.client_exceptions import ClientConnectorDNSError, ContentTypeError
 from src.models import GeocodingResponse, PriceResponse
 
 
 class APIClient:
-    
     batch_size = 50
-    rate_limit = 75  # requests per second
+    rate_limit = 100  # requests per second
     rate_limit_interval = (batch_size / rate_limit) * 2  # seconds between requests
 
-    def __init__(self, geoapi_key, priceapi_key):
+    def __init__(self, geoapi_key: str, priceapi_key: str):
         self.logger = logging.getLogger(self.__class__.__name__)
         self.geo_api_key = geoapi_key
         self.price_api_key = priceapi_key
-            
-    async def fetch_geocoding_data(self, base_url: str, geo_obj: Dict) -> GeocodingResponse:
-        headers = {'X-Api-Key': f"{self.geo_api_key}"}
+
+    async def _make_request(self, url: str, headers: Dict[str, str]) -> Dict:
+        """Helper method to make HTTP GET requests."""
         async with aiohttp.ClientSession() as session:
-            param = 'postal_code' if geo_obj['id'] == 'no_hd_geo_id_applicable' else 'city'
-            url = f"{base_url}&{param}={geo_obj['name']}"
             try:
                 async with session.get(url, headers=headers) as response:
-                    res = await response.json()
-                    # Select the first match from the responses because it has the lowest geographic granularity
-                    data = res.get('items', {}).get('aviv', {})[0].get('match', {})
-                    return GeocodingResponse(geo_obj['name'], geo_obj['id'], **data)
-            except aiohttp.client_exceptions.ClientConnectorDNSError as e:
-                self.logger.error(f"Please check the connection well-connected via Cloudflare. Error: {e}")
+                    if response.status in {200, 404}:
+                        return await response.json()
+                    else:
+                        self.logger.error(f"Unexpected status {response.status} for URL: {url}")
+                        return {}
+            except (ClientConnectorDNSError, ContentTypeError) as e:
+                self.logger.error(f"Connection error for URL: {url}. Error: {e}")
+                return {}
+
+    async def fetch_geocoding_data(self, base_url: str, geo_obj: Dict) -> GeocodingResponse:
+        headers = {'X-Api-Key': self.geo_api_key}
+        param_key = 'postal_code' if geo_obj['id'] == 'no_hd_geo_id_applicable' else 'city'
+        url = f"{base_url}&{param_key}={geo_obj['name']}"
+
+        response = await self._make_request(url, headers)
+        if response:
+            data = self._validate_geocoding_data(response.get('items', {}).get('aviv', []))
+            if data:
+                return GeocodingResponse(geo_obj['name'], geo_obj['id'], **data)
+
+        self.logger.error(f"Failed to fetch geocoding data for geo_obj: {geo_obj}")
+        return
 
     async def fetch_price_data(self, base_url: str, geoid: str, price_date: str) -> PriceResponse:
-        headers = {'X-Api-Key': f"{self.price_api_key}"}
-        async with aiohttp.ClientSession() as session:
-            url = f"{base_url}/{geoid}?price_date={price_date}"
-            no_entity_placeholder = {
-                "aviv_geo_id": geoid, "price_date": price_date, "transaction_type": "TRANSACTION_TYPE.SELL",
-                "house_price": {}, "apartment_price": {}, "hybrid_price": {}
-            }
-            try:
-                async with session.get(url, headers=headers) as response:
-                    if response.status == 404:
-                        return PriceResponse(**no_entity_placeholder)
-                    res = await response.json()
-                    data = res.get('items', {})[0]
-                    return PriceResponse(**data)
-            except aiohttp.client_exceptions.ClientConnectorDNSError as e:
-                self.logger.error(f"Please check the connection well-connected via Cloudflare. Error: {e}")
+        headers = {'X-Api-Key': self.price_api_key}
+        url = f"{base_url}/{geoid}?price_date={price_date}"
+
+        response = await self._make_request(url, headers)
+        if response:
+            if response.get('items'):
+                data = response['items'][0]
+                return PriceResponse(**data)
+
+        self.logger.error(f"Failed to fetch price data for geoid: {geoid}, price_date: {price_date}")
+        return self._default_price_response(geoid, price_date)
+    
+    @staticmethod
+    def _default_price_response(geoid: str, price_date: str) -> PriceResponse:
+        """Generate a default PriceResponse when no entity is found."""
+        return PriceResponse(
+            place_id=geoid,
+            price_date=price_date,
+            transaction_type="TRANSACTION_TYPE.SELL",
+            house_price={},
+            apartment_price={},
+            hybrid_price={}
+        )
+
+    def _validate_geocoding_data(self, items: List[Dict]) -> Dict:
+        """
+        Validate geocoding data to select the appropriate unit.
+
+        :param items: List of geocoding response items.
+        :return: Validated geocoding data dictionary or empty dictionary if none found.
+        """
+        if not items:
+            return {}
+
+        # Default to the first unit
+        selected_unit = items[0]
+        
+        # Check for 'type_key' and adjust selection if necessary
+        if selected_unit.get('match').get('type_key') == 'POCO' and len(items) > 1:
+            selected_unit = items[1]
+
+        return selected_unit.get('match', {})
